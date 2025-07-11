b21eb4a495a3d151a33327fbc5b8a7c9e7a4a82e
Raneesh Gomez <raneesh@calcey.com>
2025-07-11 19:10:47 +0530

    chore: minor refactor

--- app/routes/main.py
+++ app/routes/main.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 main_bp = Blueprint('main', __name__, url_prefix='/api/v1')
 
-@main_bp.route('/')
+@main_bp.route('/', methods=['GET'])
 def index():
     return "Welcome to the Flask TODO API"
